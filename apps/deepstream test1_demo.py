b69b54a249a346c95149005aa15b58536555baef
Akshan2 <124971884+Akshan2@users.noreply.github.com>
2023-02-10 13:02:35 +0530

    Update and rename deepstream test1_demo to deepstream test1_demo.py

---
+++ apps/deepstream test1_demo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import gi
+import pyds
+sys.path.append(../)
+gi.require_version('Gst', '1.0')
+from gi.repository import Glib, Gst
+from common.bus_call import bus_call
+from common.is_aarch_64 import is_aarch64
+
+def osd_sink_pad_buffer_probe(pad, info, u_data):
+  
+   gst_buffer = info.get_buffer()
+   batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+   l_frame = batch_meta.frame_meta_list
+   
+   while l_frame is not None:
+      try:
+          frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+      except StopIteration:
+          break
+          
+       frame_number = frame_meta.frame_num
+       l_obj = frame_meta.obj_meta_list
+        
+        while l_obj is not None:
+            try:
+                obj_meta = pyds.NvDsObjectMeta(l_obj.data)
+            except StopIteration:
+                break
+                
+            
+     
